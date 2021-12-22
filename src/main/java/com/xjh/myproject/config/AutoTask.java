90ff16ab965d5a2c5c8ed6225325b0fec25baf2c
向嘉晖 <2464674651@qq.com>
2021-12-22 15:12:06 +0800

    first commit

--- src/main/java/com/xjh/myproject/config/AutoTask.java
+++ src/main/java/com/xjh/myproject/config/AutoTask.java
@@ -1,16 +1,19 @@
-//package com.xjh.myproject.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.scheduling.annotation.Scheduled;
-//
-//@Configuration
-//@EnableScheduling
-//public class AutoTask {
-//
-//    @Scheduled(cron = "*/1 * * * * ?")
-//    public void doTask(){
-//        System.out.println("=========我是定时任务,每秒执行一次==============");
-//    }
-//
-//}
+package com.xjh.myproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * @author xjh
+ */
+@Configuration
+@EnableScheduling
+public class AutoTask {
+
+    @Scheduled(cron = "*/1 * * * * ?")
+    public void doTask(){
+        System.out.println("=========我是定时任务,每秒执行一次==============");
+    }
+
+}
